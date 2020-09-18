790716219437035d56c0e758cc120490ae61e2c7
Jeremiah <williamsjeremiahkenneth@gmail.com>
2020-09-18 15:46:51 -0400

    Make Change project completed.

--- src/main/java/com/techelevator/MakeChange.java
+++ src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -12,7 +14,36 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
-
+		
+		Scanner scannerCost = new Scanner(System.in);
+		Scanner scannerTotal = new Scanner(System.in);
+		
+		System.out.print("Welcome to Change Calculator!");
+		
+		System.out.println();
+		System.out.println();
+		
+		
+		System.out.println("Please enter your total cost: ");
+		String userInputCost = scannerCost.nextLine();
+		double userCost = Double.parseDouble(userInputCost);
+		
+		System.out.println();
+		
+		System.out.print("Please enter cash given: ");
+		String userInputMoney = scannerTotal.nextLine();
+		double userMoney = Double.parseDouble(userInputMoney);
+		
+		
+		double changeGiven = 0;
+		changeGiven = userMoney - userCost;
+		
+		System.out.println();
+		System.out.println("Your Change is: $" + changeGiven);
+		
+		scannerCost.close();
+		scannerTotal.close();
+		
 	}
 
 }
